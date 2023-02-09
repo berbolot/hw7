7f71d7408525b0a8894cb356281c91c4ee7e8ed7
berbolot <berbolotd@gmail.com>
2023-02-09 17:56:02 +0500

    first commit

---
+++ hw7-2/app.js
@@ -0,0 +1,65 @@
+const freeFire1 = document.querySelector('.fen1')
+const free1 = document.querySelector('.otvet1')
+freeFire1.addEventListener('click', function(){
+    free1.classList.toggle('otvet1');
+    if(freeFire1.innerHTML === "Скрыть ответ"){
+        freeFire1.innerHTML = "Показать ответ";
+    }else{
+        freeFire1.innerHTML = "Скрыть ответ"
+    }
+})
+
+const freeFire2 = document.querySelector('.fen2')
+const free2 = document.querySelector('.otvet2')
+freeFire2.addEventListener('click', function(){
+    free2.classList.toggle('otvet2');
+    if(freeFire2.innerHTML === "Скрыть ответ"){
+        freeFire2.innerHTML = "Показать ответ";
+    }else{
+        freeFire2.innerHTML = "Скрыть ответ"
+    }
+})
+
+const freeFire3 = document.querySelector('.fen3')
+const free3 = document.querySelector('.otvet3')
+freeFire3.addEventListener('click', function(){
+    free3.classList.toggle('otvet3');
+    if(freeFire3.innerHTML === "Скрыть ответ"){
+        freeFire3.innerHTML = "Показать ответ";
+    }else{
+        freeFire3.innerHTML = "Скрыть ответ"
+    }
+})
+
+const freeFire4 = document.querySelector('.fen4')
+const free4 = document.querySelector('.otvet4')
+freeFire4.addEventListener('click', function(){
+    free4.classList.toggle('otvet4');
+    if(freeFire4.innerHTML === "Скрыть ответ"){
+        freeFire4.innerHTML = "Показать ответ";
+    }else{
+        freeFire4.innerHTML = "Скрыть ответ"
+    }
+})
+
+const freeFire5 = document.querySelector('.fen5')
+const free5 = document.querySelector('.otvet5')
+freeFire5.addEventListener('click', function(){
+    free5.classList.toggle('otvet5');
+    if(freeFire5.innerHTML === "Скрыть ответ"){
+        freeFire5.innerHTML = "Показать ответ";
+    }else{
+        freeFire5.innerHTML = "Скрыть ответ"
+    }
+})
+
+const freeFire6 = document.querySelector('.fen6')
+const free6 = document.querySelector('.otvet6')
+freeFire6.addEventListener('click', function(){
+    free6.classList.toggle('otvet6');
+    if(freeFire6.innerHTML === "Скрыть ответ"){
+        freeFire6.innerHTML = "Показать ответ";
+    }else{
+        freeFire6.innerHTML = "Скрыть ответ"
+    }
+})
